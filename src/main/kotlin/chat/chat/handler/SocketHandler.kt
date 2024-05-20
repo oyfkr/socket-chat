88e5c1ad14b28dd5652e131982bcc144b30420c1
오근혁 <oyfkr@naver.com>
2024-05-20 23:31:12 +0900

    [신규] 소켓 채팅 프로젝트 생성 및 SocketHandler 생성

---
+++ src/main/kotlin/chat/chat/handler/SocketHandler.kt
@@ -0,0 +1,29 @@
+package chat.chat.handler
+
+import org.springframework.stereotype.Component
+import org.springframework.web.socket.CloseStatus
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.handler.TextWebSocketHandler
+
+@Component
+class SocketHandler(
+    private val sessionList: ArrayList<WebSocketSession> = arrayListOf()
+) : TextWebSocketHandler() {
+
+    override fun handleTextMessage(session: WebSocketSession, message: TextMessage) {
+        sessionList.forEach {webSocketSession ->
+            if (webSocketSession.isOpen) {
+                webSocketSession.sendMessage(TextMessage(message.payload))
+            }
+        }
+    }
+
+    override fun afterConnectionEstablished(session: WebSocketSession) {
+        sessionList.add(session)
+    }
+
+    override fun afterConnectionClosed(session: WebSocketSession, status: CloseStatus) {
+        sessionList.remove(session)
+    }
+}
